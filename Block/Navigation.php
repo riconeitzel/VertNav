3157f5739417991a62e25db4f1fbb6f213281b18
vinai <vinai@netzarbeiter.com>
2009-09-23 12:42:30 +0200

    Added new css clases for easier formatting

--- Block/Navigation.php
+++ Block/Navigation.php
@@ -34,7 +34,7 @@
  */
 class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
 {
-	
+
 	/**
 	 * Add the customer group to the cache key so this module is compatible with more extensions.
 	 * Netzarbeiter_GroupsCatalog
@@ -48,7 +48,7 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
         $key .= $this->_getCustomerGroupId();
         return $key;
     }
-    
+
     /**
      * check if we should hide the categories because of Netzarbeiter_LoginCatalog
      *
@@ -58,7 +58,7 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
     {
     	return $this->_isLoginCatalogInstalledAndActive() && $this->_loginCatalogHideCategories();
     }
-    
+
     /**
      * Check if the Netzarbeter_LoginCatalog extension is installed and active
      *
@@ -72,7 +72,7 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
     	}
     	return false;
     }
-    
+
     /**
      * Check if the Netzarbeter_LoginCatalog extension is configured to hide categories from logged out customers
      *
@@ -87,7 +87,7 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
 		}
 		return false;
     }
-    
+
     /**
      * This method is only here to provide compatibility with the Netzarbeter_LoginCatalog extension
      *
@@ -101,7 +101,7 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
         if ($this->_checkLoginCatalog()) return '';
         return parent::drawItem($category, $level, $last);
     }
-    
+
     /**
      * Add project specific formatting
      *
@@ -113,35 +113,40 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
     public function drawOpenCategoryItem($category, $level=0, array $class=null)
     {
         $html = '';
-        
+
     	//if (! $category instanceof Varien_Data_Tree_Node) return $html;
-    	
+
         if ($this->_checkLoginCatalog()) return $html;
-        
+
         if (! $category->getIsActive()) return $html;
-        
+
         if (! isset($class)) $class = array();
-        
-        $class[] = 'level' . $level;
-        $class[] = $this->_getClassNameFromCategoryName($category);
+		$classColl = array();
+
+        $classColl[] = 'level' . $level;
         if ($this->_isCurrentCategory($category))
         {
-        	$class[] = 'active';
+        	$classColl[] = 'active';
         }
         else
         {
-			$class[] = $this->isCategoryActive($category) ? 'parent' : 'inactive';
+			$classColl[] = $this->isCategoryActive($category) ? 'parent' : 'inactive';
         }
-    
+		$class[] = implode('-', $classColl);
+
+		$class = array_merge($class, $classColl);
+
+        $class[] = $this->_getClassNameFromCategoryName($category);
+
         $productCount = '';
         if (Mage::getStoreConfig('catalog/vertnav/display_product_count'))
         {
         	$productCount = '<span class="product-count"> (' . $this->_getProductCount($category) . ')</span>';
         }
-        
+
         // indent HTML!
         $html .= str_pad ( "", ($level * 2 ) + 2, " " ).sprintf('<li class="%s">', implode(" ", $class))."\n";
-        
+
         $html .= str_pad ( "", (($level * 2 ) + 4), " " ).'<span class="vertnav-cat"><a href="'.$this->getCategoryUrl($category).'"><span>'.$this->htmlEscape($category->getName()).'</span></a>'.$productCount."</span>\n";
 
         if (in_array($category->getId(), $this->getCurrentCategoryPath()))
@@ -154,13 +159,13 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
         	{
         		$children = $category->getChildren();
         	}
-            
+
             $hasChildren = $children && ($childrenCount = count($children));
             if ($hasChildren)
             {
             	//$children = $this->toLinearArray($children);
                 $htmlChildren = '';
-				
+
                 foreach ($children as $i => $child)
                 {
                 	$class = array();
@@ -191,11 +196,11 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
         $html.= "\n".str_pad ( "", ($level * 2 ) + 2, " " ).'</li>'."\n";
         return $html;
     }
-    
+
     /**
      * I need an array with the index being continunig numbers, so
      * it's possible to check for the previous/next category
-     * 
+     *
      * @param mixed $collection
      * @return array
      */
@@ -205,24 +210,16 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
     	foreach ($collection as $item) $array[] = $item;
     	return $array;
     }
-    
+
     protected function _getClassNameFromCategoryName($category)
     {
-    	$name = strtolower($category->getName());
-		/*
-		 * Handle special german characters
-		 */
-		$s = array('ö', 'ü', 'ä', 'ß');
-		$r = array('oe', 'ue', 'ae', 'ss');
-		$name = str_replace($s, $r, $name);
-		
-    	$name = preg_replace('/-{2,}/', '-', preg_replace('/[^a-z-]/', '-', $name));
+    	$name = $category->getName();
+    	$name = preg_replace('/-{2,}/', '-', preg_replace('/[^a-z-]/', '-', strtolower($name)));
 		while ($name && $name{0} == '-') $name = substr($name, 1);
 		while ($name && substr($name, -1) == '-') $name = substr($name, 0, -1);
-		
     	return $name;
     }
-	
+
 	/**
 	 * Return the current customer group id. Logged out customers get the group id 0,
 	 * not the default set in system > config > customers
@@ -236,12 +233,12 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
 		else $customerGroupId = $session->getCustomerGroupId();
 		return $customerGroupId;
 	}
-	
+
 	protected function _isCurrentCategory($category)
 	{
 		return ($cat = $this->getCurrentCategory()) && $cat->getId() == $category->getId();
 	}
-	
+
 	protected function _getProductCount($category)
 	{
 		$count = 0;
@@ -255,7 +252,7 @@ class RicoNeitzel_VertNav_Block_Navigation extends Mage_Catalog_Block_Navigation
 		}
 		return $count;
 	}
-	
+
 	protected function _getProductCountFromTreeNode(Varien_Data_Tree_Node $category)
 	{
 		return Mage::getModel('catalog/category')->setId($category->getId())->getProductCount();
