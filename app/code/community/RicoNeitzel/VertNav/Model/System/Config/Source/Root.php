aea34923c838ccac2184e90f0897848cddda8aff
Vinai Kopp <vinai@netzarbeiter.com>
2011-07-01 10:22:00 +0200

    Add and update license header comments

--- app/code/community/RicoNeitzel/VertNav/Model/System/Config/Source/Root.php
+++ app/code/community/RicoNeitzel/VertNav/Model/System/Config/Source/Root.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0).
+ * It is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future.
+ *
+ * @category   RicoNeitzel
+ * @package    RicoNeitzel_VertNav
+ * @copyright  Copyright (c) 2011 Vinai Kopp http://netzarbeiter.com/
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class RicoNeitzel_VertNav_Model_System_Config_Source_Root
 	extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
